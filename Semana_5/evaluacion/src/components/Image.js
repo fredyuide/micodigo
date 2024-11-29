e2b0f8dfe3d70d67fd6c8457849ad73a122691ee
fredyuide <frguamango@uide.edu.ec>
2024-11-29 09:00:20 -0500

    Evaluacion

---
+++ Semana_5/evaluacion/src/components/Image.js
@@ -0,0 +1,6 @@
+
+function Image({parametro_logo}) {
+  return <img src={parametro_logo} className="App-logo" alt="logo" />;
+}
+
+export default Image;
---
+++ Semana_5/evaluacion/src/components/Input.js
@@ -0,0 +1,6 @@
+
+function Input({ id, type, onChange, required }) {
+  return <input id={id} type={type} onChange={onChange} required={required} />;
+}
+
+export default Input;
