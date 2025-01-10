8d300914834f2a594ed5040c482af3e800eab976
Freddy Guaman <frguamango@uide.edu.ec>
2025-01-10 08:43:58 -0500

    CRUD TEACHER
    CRUD TEACHER

--- proyectoClase/back/routes/teacher.routes.js
+++ proyectoClase/back/routes/teacher.routes.js
@@ -5,14 +5,44 @@ const teacherController = require("../controllers/teacher.controller")
 
 /* POST METHOD CREATE USER. */
 router.get('/list', function(require, response) {
-
     const result = teacherController.getList();
+    console.log(result);
 
     response.status(200).json({
         data: result,
         status: true,
         message: "Teachers message",
     })
+});
+
+router.post("/create", function(request, response) {
+    console.log(request.body);
+    const result = teacherController.postCreate(request.body);
+
+    response.status(200).json({
+        status: true,
+        info: result,
+    });
+});
+
+router.post("/update", function(request, response) {
+    console.log(request.body);
+    const result = teacherController.postUpdate(request.body);
+
+    response.status(200).json({
+        status: true,
+        info: result,
+    });
+});
+
+router.post("/delete", function(request, response) {
+    console.log(request.body);
+    const result = teacherController.postDelete(request.body);
+
+    response.status(200).json({
+        status: true,
+        info: result,
+    });
 });
 
 module.exports = router;
